b58fb8cd64e7f585e7368c7a56ecb102a66dd93a
Anwesh43 <anweshthecool0@gmail.com>
2019-03-24 12:37:10 +0530

    adding SSBNode class

--- seesawbasicview/src/main/java/com/anwesh/uiprojects/seesawbasicview/SeeSawBasicView.kt
+++ seesawbasicview/src/main/java/com/anwesh/uiprojects/seesawbasicview/SeeSawBasicView.kt
@@ -117,4 +117,49 @@ class SeeSawBasicView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SSBNode(var i : Int, val state : State = State()) {
+
+        private var prev : SSBNode? = null
+        private var next : SSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSSBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SSBNode {
+            var curr : SSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+
+    }
 }
