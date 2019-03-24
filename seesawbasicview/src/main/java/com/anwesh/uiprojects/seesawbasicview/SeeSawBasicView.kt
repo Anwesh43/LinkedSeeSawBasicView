13adf2b513afc643e4db4fc3d67d4e7759405891
Anwesh43 <anweshthecool0@gmail.com>
2019-03-24 12:30:31 +0530

    adding seesawnbasicview module

--- seesawbasicview/src/main/java/com/anwesh/uiprojects/seesawbasicview/SeeSawBasicView.kt
+++ seesawbasicview/src/main/java/com/anwesh/uiprojects/seesawbasicview/SeeSawBasicView.kt
@@ -52,3 +52,21 @@ fun Canvas.drawSSBNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class SeeSawBasicView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
